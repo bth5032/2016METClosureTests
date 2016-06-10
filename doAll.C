f82382fe18b4e16313d2273a14990129d64f9e19
Bobak <bth5032@gmail.com>
2016-06-10 17:21:26 +0200

    minor renamed a file

--- doAll.C
+++ doAll.C
@@ -1,6 +1,6 @@
 #include "ScanChain.C"
 #include "DefineDatasets.C"
-#include "ConfigParser/ConfigParser.C"
+#include "ConfigParser.C"
 
 void doAll(TString config_name, TString config_file="run_modes.conf"){
 
