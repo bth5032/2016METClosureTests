7c3f2b2cf181a2545dad839ee2e4f15fb4878ad6
Bobak <bth5032@gmail.com>
2016-06-13 11:29:30 +0200

    Minor, funcs.sh now work for plots too

--- funcs.sh
+++ funcs.sh
@@ -11,15 +11,10 @@ function makeHistos {
 	root -l -b -q "doAll.C(\"$1\")"
 }	
 
-function makeDirectories {
-	if [[ ! -d $CT_PLOTS_OUTPUT_DIR ]]
+function mkplotdir {
+	if [[ ! -d $1 ]]
 	then
-		mkdir -p $CT_PLOTS_OUTPUT_DIR
-		cp ~/public_html/ZMET2016/index.php ${CT_PLOTS_OUTPUT_DIR}/
-	fi
-
-	if [[ ! -d $CT_HISTO_DIR ]]
-	then
-		mkdir -p $CT_HISTO_DIR
+		mkdir -p $1
+		cp ~/public_html/ZMET2016/index.php ${1}/
 	fi
 }
