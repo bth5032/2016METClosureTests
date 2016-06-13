86f33cec21aaeabb40515d6322532c7418f7ee27
Bobak <bth5032@gmail.com>
2016-06-13 11:38:46 +0200

    added bit to make reweight hist

--- doAll.C
+++ doAll.C
@@ -2,9 +2,9 @@
 #include "ScanChain.C"
 #include "DefineDatasets.C"
 #include "ConfigParser.C"
+#include "makeWeightHisto.C"
 
 void doAll ( TString config_name, TString config_file="configs/run_modes.conf" ) {
-
 
   ConfigParser *conf = new ConfigParser(config_file.Data());
 
@@ -12,6 +12,12 @@ void doAll ( TString config_name, TString config_file="configs/run_modes.conf" )
 
     cout<<"Using config:"<<endl;
     conf->print();
+
+    if (conf->get("mode") == "rwt"){
+      TString out_dir=conf->get("histo_output_dir");
+      makeWeightHisto(out_dir,out_dir+"/ct_zjets_base.root", out_dir+"/ct_gjets_base.root", "vpt");
+    }
+
     if (conf->get("zjets") == "true") {
       ScanChain(getZJetsChain(conf->get("data_set")), "zjets", conf); 
     }
