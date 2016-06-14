9e383bad6ee462ba69ec2cb6f4043a0b6b9a516d
Bobak <bth5032@gmail.com>
2016-06-14 18:46:05 +0200

    Changed reweight histogram to TH1D

--- makeWeightHisto.C
+++ makeWeightHisto.C
@@ -36,12 +36,12 @@ void makeWeightHisto(TString output_dir, TString primary_histos, TString seconda
   h_secondary.Scale(1./h_secondary.GetSumOfWeights());
   h_primary.Scale(1./h_primary.GetSumOfWeights());
 
-  TH1D * h_ratio = (TH1D*) h_primary.Clone("h_"+hist_name+"_ratio");
-  h_ratio->Divide(h_secondary);
+  TH1D h_ratio = (TH1D) h_primary.Clone("h_"+hist_name+"_ratio");
+  h_ratio.Divide(h_secondary);
 
   TFile * file = TFile::Open(output_dir+hist_name+"_ratio.root","RECREATE");
   file->cd();
-  h_ratio->Write();
+  h_ratio.Write();
   h_primary.Write();
   h_secondary.Write();
   file->Close();
