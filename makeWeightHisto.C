7cb524d1c47ee1fb6b79cd350eb782922a647745
Bobak <bth5032@gmail.com>
2016-06-16 19:52:40 +0200

    Added unscaled ratio hist to histo file

--- makeWeightHisto.C
+++ makeWeightHisto.C
@@ -22,6 +22,8 @@ void makeWeightHisto(ConfigParser * conf)
   TFile * f_secondary = TFile::Open(secondary_histos, "READ"); //typically location to zjets hist
   TH1D * h_primary;
   TH1D * h_secondary;
+  TH1D * h_primary_scaled;
+  TH1D * h_secondary_scaled;
 
   if (f_primary && f_secondary) {
     h_primary = (TH1D*)f_primary->Get(primary_name+"_"+hist_name)->Clone("h_"+primary_name);
@@ -35,11 +37,13 @@ void makeWeightHisto(ConfigParser * conf)
   TH1D * h_ratio_unscaled = (TH1D*) h_primary->Clone("h_"+hist_name+"_ratio_unscaled");
   h_ratio_unscaled->Divide(h_secondary); 
 
-  h_secondary->Scale(1./h_secondary->GetSumOfWeights());
-  h_primary->Scale(1./h_primary->GetSumOfWeights());
+  h_primary_scaled=(TH1D*) h_primary->Clone("h_"+primary_name+"_scaled");
+  h_secondary_scaled=(TH1D*) h_secondary->Clone("h_"+secondary_name+"_scaled");
+  h_secondary_scaled->Scale(1./h_secondary->GetSumOfWeights());
+  h_primary_scaled->Scale(1./h_primary->GetSumOfWeights());
 
-  TH1D * h_ratio = (TH1D*) h_primary->Clone("h_"+hist_name+"_ratio");
-  h_ratio->Divide(h_secondary);
+  TH1D * h_ratio = (TH1D*) h_primary_scaled->Clone("h_"+hist_name+"_ratio");
+  h_ratio->Divide(h_secondary_scaled);
 
   TFile * file = TFile::Open(output_location,"RECREATE");
   file->cd();
@@ -47,6 +51,8 @@ void makeWeightHisto(ConfigParser * conf)
   h_ratio_unscaled->Write();
   h_primary->Write();
   h_secondary->Write();
+  h_primary_scaled->Write();
+  h_secondary_scaled->Write();
   file->Close();
 
   cout<<"Reweight histogram succesfully made at "<<output_location<<endl;
