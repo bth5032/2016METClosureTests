bd77e96d894c7081c91d020f941782bf9822e7bc
Bobak <bth5032@gmail.com>
2016-07-06 13:29:24 +0200

    added pileup reweighting for rare smp

--- ScanChain.C
+++ ScanChain.C
@@ -707,7 +707,7 @@ int ScanChain( TChain* chain, TString sampleName, ConfigParser *configuration, b
   }
 
   if( conf->get("rares") == "true" ){
-    cout<<"Pileup reweighting with nvtx_ratio_4p0fb.root")<<endl;
+    cout<<"Pileup reweighting with nvtx_ratio_4p0fb.root"<<endl;
     g_pileup_hist_file = TFile::Open("nvtx_ratio_4p0fb.root", "READ");
     g_pileup_hist = (TH1D*)g_weight_hist_file->Get("h_vtx_ratio")->Clone("h_pileup_weight");
     g_pileup_hist->SetDirectory(rootdir);
