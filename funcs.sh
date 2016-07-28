6240eeaec8a2759f44603cf23e9216f196b6351c
Bobak Hashemi <bth5032@gmail.com>
2016-07-27 19:37:51 -0700

    more support for HT reweighting just to A_Btag, added max event counter

--- funcs.sh
+++ funcs.sh
@@ -69,9 +69,7 @@ function makeAllForDir {
 function makeHistosForDir {
 	if [[ -a $1/run_modes.conf ]]
 	then
-		makeHistos Z_Base $1/run_modes.conf
-		makeHistos G_Base $1/run_modes.conf
-		makeHistos G_Reweight $1/run_modes.conf
+		makeHistos all $1/run_modes.conf
 	else
 		echo "Can not find $1/run_modes.conf"
 	fi
