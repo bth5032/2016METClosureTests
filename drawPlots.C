64e12f33ce2496469b22975027a35c701970a19a
Bobak <bth5032@gmail.com>
2016-08-08 15:09:07 -0700

    removed trace statements, the thing runs at least

--- drawPlots.C
+++ drawPlots.C
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-vector<int> ROOTCOLORPALATE = {46,8,9,30,38,40,2,6,28,42,3,5,7,41};
+vector<int> ROOTCOLORPALATE = {46,8,9,38,40,2,30,6,28,42,3,5,7,41};
 
 double errMult(double A, double B, double errA, double errB, double C) {
   return sqrt(C*C*(pow(errA/A,2) + pow(errB/B,2)));
@@ -228,7 +228,7 @@ TString drawArbitraryNumberWithResidual(ConfigParser *conf){
   //===========================
   //cout<<__LINE__<<endl;
   for (int i = 1; i<num_hists; i++){
-    hists[i]->SetFillColor(ROOTCOLORPALATE[i % ROOTCOLORPALATE.size()]);
+    hists[i]->SetFillColor(ROOTCOLORPALATE[(i-1) % ROOTCOLORPALATE.size()]);
     hists[i]->SetFillStyle(1001);
   }
   //cout<<__LINE__<<endl;
