d31c9f0e1989665beaf476e2de1cb93fdb32c06a
Bobak <bth5032@gmail.com>
2016-08-12 16:00:04 -0700

    fixed bg normalizing

--- drawPlots.C
+++ drawPlots.C
@@ -254,7 +254,7 @@ TString drawArbitraryNumberWithResidual(ConfigParser *conf){
     //rescale everything to scale factor
     scaleFactor = ((double) numEventsData/numEventsMC);
     for (int i = 1; i<num_hists; i++){
-      if (hist_nums_for_norm.Contains(to_string(i)) || hist_nums_for_norm == "" ) hists[i]->Scale(scaleFactor);  
+      if (hist_nums_for_norm.Contains(to_string(i)) || hist_nums_for_norm == "" ) hists[i]->Scale(scaleFactor);  //if hist is marked for norm or no hists marked for norms.
     }
   }
 
