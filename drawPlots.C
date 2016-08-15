dc8cd7b758da4805952954c51acbaf86fa44e1c6
Bobak <bth5032@gmail.com>
2016-08-15 13:28:08 -0700

    fixed bg normalizing

--- drawPlots.C
+++ drawPlots.C
@@ -384,7 +384,7 @@ TString drawArbitraryNumberWithResidual(ConfigParser *conf){
       cout<<"STATS: "<<stats_string<<endl;
       drawLatexFromTString(stats_string, .52,.5+(0.02*i));
     }
-    stats_string = "Sum of BG Events from "+conf->get("stats_low_val")+" to "+conf->get("stats_high_val")+" : "+to_string(num_evts_in_interval_sum)+" Error: "+to_string(err_evts_in_interval_sum)+" Ratio: "+to_string(double num_evts_in_interval_sum/num_evts_in_interval_primary);
+    stats_string = "Sum of BG Events from "+conf->get("stats_low_val")+" to "+conf->get("stats_high_val")+" : "+to_string(num_evts_in_interval_sum)+" Error: "+to_string(err_evts_in_interval_sum)+" Ratio: "+to_string((double) num_evts_in_interval_sum/num_evts_in_interval_primary);
     cout<<"STATS: "<<stats_string<<endl;
     drawLatexFromTString(stats_string, .52,.5+(0.02*num_hists));
     //cout<<__LINE__<<endl;
