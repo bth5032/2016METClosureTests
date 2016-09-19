d8f7f535b1fd8853eec7f37700b03e4fe4e89c9f
Bobak Hashemi <bth5032@gmail.com>
2016-09-19 14:13:08 -0700

    testing error pipeline

--- computeErrors.C
+++ computeErrors.C
@@ -123,10 +123,15 @@ vector<double> getMetTemplatesError(vector<double> stat_err, vector<double> bin_
   double err_bin; //error in bin
 
   for (int i=0; i<stat_err.size(); i++){
+    cout<<"TRACE| Bin "<<i<<" ";
     err_bin = stat_err[i]*stat_err[i]; //Statistical Error
+    cout<<"Stat Error: "<< stat_err[i]
     err_bin += bin_count[i]*bin_count[i]*MC_Closure_Error[i]*MC_Closure_Error[i]; //Closure Error
+    cout<<" Closure Error: "<<bin_count[i]*MC_Closure_Error[i];
     err_bin += ((sqrt(normalization)*bin_count[i])/(normalization))*((sqrt(normalization)*bin_count[i])/(normalization)); //Normalization of Zjets
-    err_bin += EWK_Error[i]*EWK_Error[i]; //Normalization of Zjets
+    cout<<" Normalization: "<<((sqrt(normalization)*bin_count[i])/(normalization));
+    err_bin += EWK_Error[i]*EWK_Error[i]; //EWK Subtraction
+    cout<<" EWK Subtraction: "<<EWK_Error[i]<<endl;
 
     output_errors.push_back(sqrt(err_bin));
   }
