234b1b7e9105a0df7be21d791983a56a3ccd6c04
Bobak Hashemi <bth5032@gmail.com>
2016-09-18 21:28:51 -0700

    testing error pipeline

--- drawPlots.C
+++ drawPlots.C
@@ -377,50 +377,50 @@ TString drawArbitraryNumberWithResidual(ConfigParser *conf){
     vector<double> FS_count;
 
     vector<double> signal_count;
-    cout<<__LINE__<<endl;
+    //cout<<__LINE__<<endl;
 
     //Fill in all the bin counts here
     for (int i = 0; i < stats_bins.size(); i++){
       signal_count.push_back(hists[0]->Integral(hists[0]->FindBin(stats_bins[i].first), hists[0]->FindBin(stats_bins[i].first - 0.001)));
       FS_count.push_back(hists[5]->Integral(hists[5]->FindBin(stats_bins[i].first), hists[5]->FindBin(stats_bins[i].first - 0.001)));
       
-      cout<<__LINE__<<endl;
+      //cout<<__LINE__<<endl;
       
       template_count.push_back(hists[6]->IntegralAndError(hists[6]->FindBin(stats_bins[i].first), hists[6]->FindBin(stats_bins[i].first - 0.001), t_err));
       template_error.push_back(t_err);
       
-      cout<<__LINE__<<endl;
+      //cout<<__LINE__<<endl;
       
       rare_count.push_back(hists[1]->IntegralAndError(hists[1]->FindBin(stats_bins[i].first), hists[1]->FindBin(stats_bins[i].first - 0.001), r_err));
       rare_error.push_back(r_err);
 
-      cout<<__LINE__<<endl;
+      //cout<<__LINE__<<endl;
 
       rare_count[i] += hists[2]->IntegralAndError(hists[2]->FindBin(stats_bins[i].first), hists[2]->FindBin(stats_bins[i].first - 0.001), r_err);
       rare_error[i] += r_err;
 
-      cout<<__LINE__<<endl;
+      //cout<<__LINE__<<endl;
 
       rare_count[i] += hists[3]->IntegralAndError(hists[3]->FindBin(stats_bins[i].first), hists[3]->FindBin(stats_bins[i].first - 0.001), r_err);
       rare_error[i] += r_err;
 
-      cout<<__LINE__<<endl;
+      //cout<<__LINE__<<endl;
 
       rare_count[i] += hists[4]->IntegralAndError(hists[4]->FindBin(stats_bins[i].first), hists[4]->FindBin(stats_bins[i].first - 0.001), r_err);
       rare_error[i] += r_err;
 
-      cout<<__LINE__<<endl;
+      //cout<<__LINE__<<endl;
     }
 
     vector<double> temp_err = getMetTemplatesError(template_error, template_count, normalization_error, conf->get("SR"));
-    cout<<__LINE__<<endl;
+    //cout<<__LINE__<<endl;
     pair<vector<double>,vector<double>> FS_err = getFSError(FS_count);
-    cout<<__LINE__<<endl;
+    //cout<<__LINE__<<endl;
     vector<double> rare_err = getRareSamplesError(rare_error, rare_count);
-    cout<<__LINE__<<endl;
+    //cout<<__LINE__<<endl;
 
     printCounts(template_count, temp_err, rare_count, rare_err, FS_count, FS_err, stats_bins, signal_count);
-    cout<<__LINE__<<endl;
+    //cout<<__LINE__<<endl;
 
   }
   
