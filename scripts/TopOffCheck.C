a6a09254c5da1dfebba03c4b4ea578bdce7ffe36
Bobak Hashemi <bth5032@gmail.com>
2016-10-24 00:09:33 -0700

    Added all for topoffcheck

---
+++ scripts/TopOffCheck.C
@@ -0,0 +1,34 @@
+#include "TString.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TCanvas.h"
+
+void TopOffCheck(TString dir_loc="/nfs-7/userdata/bobak/ZMET2016_Hists_PostICHEP/", out_dir="/home/users/bhashemi/public_html/TopOffCheck/"){
+	TFile *f_data = TFile::Open(dir_loc+"Data/ct_Z_Base_EdgeZ.root");
+	TFile *f_MC = TFile::Open(dir_loc+"ct_Z_Base_EdgeZ.root");
+	double sf = 0;
+//------------------
+// nVert Checking
+//------------------
+	TH1D *h_nVert_d = (TH1D*) ((TH1D*) (f_data->Get("zjets_nVert")))->Clone("h_nVert_d");
+	TH1D *h_nVert_mc = (TH1D*) ((TH1D*) (f_MC->Get("zjets_nVert")))->Clone("h_nVert_mc");
+
+	TCanvas * c_nVert = new TCanvas("c_nVert", "", 2000, 2000);
+
+	h_nVert_d->SetTitle("Number of Verticies Data Vs. MC in Zjets.");
+	h_nVert_d->Draw("E1");
+	sf = h_nVert_d->Integral()/h_nVert_MC->Integral();
+	h_nVert_mc->Scale(sf);
+	h_nVert_mc->Draw("HIST SAME");
+
+	TLegend *l_nVert;
+	l_nVert = new TLegend(0.73, 0.73, 0.88, 0.88);
+
+	l_nVert->SetLineColor(kWhite);  
+	l_nVert->SetShadowColor(kWhite);
+	l_nVert->SetFillColor(kWhite);
+	l_nVert->AddEntry(h_nVert_d, "Data", "p");
+	l_nVert->AddEntry(h_nVert_mc, "MC", "f");
+
+	c_nVert->Draw(out_dir+"nvert.png");
+}
