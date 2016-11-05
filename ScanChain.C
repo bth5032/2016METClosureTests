1d5216f5a09e622c0ca441dd290be9f88057bd83
Bobak Hashemi <bth5032@gmail.com>
2016-11-05 21:20:29 +0100

    Added configs for the ZH signal study

--- ScanChain.C
+++ ScanChain.C
@@ -1022,7 +1022,7 @@ int ScanChain( TChain* chain, TString sampleName, ConfigParser *configuration, b
   TH1D *pt_HLT_Photon30_R9Id90_HE10_IsoM = new TH1D(sampleName+"_pt_HLT_Photon30_R9Id90_HE10_IsoM", "P_{T} for HLT_Photon30_R9Id90_HE10_IsoM",6000,0,6000);
   TH1D *pt_HLT_Photon22_R9Id90_HE10_IsoM = new TH1D(sampleName+"_pt_HLT_Photon22_R9Id90_HE10_IsoM", "P_{T} for HLT_Photon22_R9Id90_HE10_IsoM",6000,0,6000);*/
 
-  TH1D * sum_mlb, m_bb_csv, m_bb_bpt, mt2j, sum_pt_z_bb;
+  TH1D *sum_mlb, *m_bb_csv, *m_bb_bpt, *mt2j, *sum_pt_z_bb;
   if(conf->get("signal_region") == "TChiZH"){
     sum_mlb = new TH1D(sampleName+"_sum_mlb", "#Sigma M_{lb} for "+sampleName, 6000,0,6000);
     sum_mlb->SetDirectory(rootdir);
