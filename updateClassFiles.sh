5d359ecd9fa8984145c154c6613c2b5123c91083
Bobak Hashemi <bth5032@gmail.com>
2016-11-05 21:11:49 +0100

    Added configs for the ZH signal study

--- updateClassFiles.sh
+++ updateClassFiles.sh
@@ -1,7 +1,7 @@
-pushd /home/users/bhashemi/Projects/GIT/Software/
+pushd /home/users/bhashemi/Projects/GIT/Software/makeCMS3ClassFiles/
 git pull 
 git checkout root6
 cd makeCMS3ClassFiles
 root -l -b -q "makeCMS3ClassFiles.C(\"/home/users/bhashemi/Projects/GIT/Vince/CMSSW_8_0_7/src/ZMET2016/dilepbabymaker/unknown_test_80_data_ee.root\", \"t\", \"ZMET2016\", \"zmet\", \"phys\")"
-mv ZMET.* /home/users/bhashemi/Projects/GIT/2016METClosureTests
+mv ZMET.* /home/users/bhashemi/Projects/GIT/2016METClosureTests/
 popd
