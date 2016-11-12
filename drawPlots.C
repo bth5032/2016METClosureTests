d2923332dcd76a0738ed1299f572e4a92a87f09d
Bobak Hashemi <bth5032@gmail.com>
2016-11-12 22:12:48 +0100

    added more MT2 plots for Frank

--- drawPlots.C
+++ drawPlots.C
@@ -1090,7 +1090,7 @@ TString drawSingleTH2(ConfigParser *conf){
   h->GetYaxis()->SetTitleOffset(1.3);
 
   h->SetTitle(plot_title);
-  gStyle->SetTitleW(.9); //title width 
+  gStyle->SetTitleW(1); //title width 
   gStyle->SetTitleH(.1); //title height 
   //h->SetTitleSize(2);
 
