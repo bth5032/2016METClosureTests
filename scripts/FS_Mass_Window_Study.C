3ce1c400ddc8dad5b792951557dab70cc16e9b61
Bobak Hashemi <bth5032@gmail.com>
2016-11-16 13:51:51 +0100

    added overflow bin to FS mass study plots

--- scripts/FS_Mass_Window_Study.C
+++ scripts/FS_Mass_Window_Study.C
@@ -28,7 +28,7 @@ void FS_Mass_Window_Study(){
   on_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_onZ/wz.root", "read"));
   on_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_onZ/zz.root", "read"));
 
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
   above_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_aboveZ/Z_Base.root", "read"));
   above_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_aboveZ/TT_dilep.root", "read"));
@@ -41,7 +41,7 @@ void FS_Mass_Window_Study(){
   above_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_aboveZ/wz.root", "read"));
   above_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_aboveZ/zz.root", "read"));
 
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
   below_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_belowZ/Z_Base.root", "read"));
   below_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_belowZ/TT_dilep.root", "read"));
@@ -55,43 +55,43 @@ void FS_Mass_Window_Study(){
   below_files.push_back(TFile::Open("/nfs-7/userdata/bobak/ZMET2016_Hists_NovemberClean/FS_mass_window_studies/baseline_belowZ/zz.root", "read"));
 
 
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
 // ==================
 //  Define Histograms
 // ==================
   TH1D *onz_met = (TH1D*) ((TH1D*) on_files[0]->Get("type1MET"))->Clone("onz_met");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   TH1D *offz_met = (TH1D*) ((TH1D*) above_files[0]->Get("type1MET"))->Clone("offz_met");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   offz_met->Add((TH1D*) ((TH1D*) below_files[0]->Get("type1MET")));
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
   TH1D *onz_njets = (TH1D*) ((TH1D*) on_files[0]->Get("njets"))->Clone("onz_njets");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   TH1D *offz_njets = (TH1D*) ((TH1D*) above_files[0]->Get("njets"))->Clone("offz_njets");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   offz_njets->Add((TH1D*) ((TH1D*) below_files[0]->Get("njets")));
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
   TH1D *onz_nbjets = (TH1D*) ((TH1D*) on_files[0]->Get("nbjets"))->Clone("onz_nbjets");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   TH1D *offz_nbjets = (TH1D*) ((TH1D*) above_files[0]->Get("nbjets"))->Clone("offz_nbjets");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   offz_nbjets->Add((TH1D*) ((TH1D*) below_files[0]->Get("nbjets")));
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
   TH1D *onz_ht = (TH1D*) ((TH1D*) on_files[0]->Get("ht"))->Clone("onz_ht");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   TH1D *offz_ht = (TH1D*) ((TH1D*) above_files[0]->Get("ht"))->Clone("offz_ht");
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   offz_ht->Add((TH1D*) ((TH1D*) below_files[0]->Get("ht")));
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 
 // ==================
 //  Fill Histograms
 // ==================
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
   for (int i = 1; i < (int) on_files.size(); i++){
     onz_met->Add((TH1D*) ((TH1D*) on_files[i]->Get("type1MET")));
     offz_met->Add((TH1D*) ((TH1D*) below_files[i]->Get("type1MET")));
@@ -109,7 +109,7 @@ void FS_Mass_Window_Study(){
     offz_ht->Add((TH1D*) ((TH1D*) below_files[i]->Get("ht")));
     offz_ht->Add((TH1D*) ((TH1D*) above_files[i]->Get("ht")));
   }
-  //cout<<__LINE__<<endl;
+  cout<<__LINE__<<endl;
 // ==================
 //  Rebin Hists
 // ==================
@@ -148,7 +148,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c1 = new TCanvas("c1", "", 2000, 2000);
     c1->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
@@ -181,7 +181,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c2 = new TCanvas("c2", "", 2000, 2000);
     c2->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
@@ -209,7 +209,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c3 = new TCanvas("c3", "", 2000, 2000);
     c3->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
@@ -239,7 +239,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c4 = new TCanvas("c4", "", 2000, 2000);
     c4->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
@@ -269,7 +269,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c5 = new TCanvas("c5", "", 2000, 2000);
     c5->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
@@ -299,7 +299,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c6 = new TCanvas("c6", "", 2000, 2000);
     c6->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
@@ -329,7 +329,7 @@ void FS_Mass_Window_Study(){
     TCanvas *c7 = new TCanvas("c7", "", 2000, 2000);
     c7->cd();
 
-    //cout<<__LINE__<<endl;
+    cout<<__LINE__<<endl;
 
     //gPad->SetLogy(1);
     gPad->SetLeftMargin(.1);
