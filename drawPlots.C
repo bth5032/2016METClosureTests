889c4e6d9499ca25e4dcfee190d90d40b82b6703
Bobak Hashemi <bth5032@gmail.com>
2016-12-01 18:39:21 +0100

    attempted fix at overflow issue

--- drawPlots.C
+++ drawPlots.C
@@ -77,8 +77,8 @@ void blindAfter(TH1D * &hist, double xmax){
   /* Sets all the bins starting from xmax to 0, count and error */
   int max_bin = hist->FindBin(xmax);
   for( int bini = max_bin; bini < hist->GetNbinsX(); bini++ ){
-    hist->SetBinContent( 0 ); 
-    hist->SetBinError  ( 0 );  
+    hist->SetBinContent( bini + 1, 0 ); 
+    hist->SetBinError  ( bini + 1, 0 );  
   }
 }
 
