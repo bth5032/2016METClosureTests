379b55064eeaef0776e1b9794cfa9b0508730a72
Bobak Hashemi <bth5032@gmail.com>
2016-12-08 14:53:59 +0100

    turned off some debugging

--- ScanChain.C
+++ ScanChain.C
@@ -1742,7 +1742,7 @@ int ScanChain( TChain* chain, ConfigParser *configuration, bool fast/* = true*/,
       eventCount++;
       
       //if (printStats) {cout<<"Event: "<<phys.evt()<<endl;}
-      cout<<"Event_Run_Lumi: "<<phys.evt()<<" "<<phys.run()<<" "<<phys.lumi()<<endl;
+      //cout<<"Event_Run_Lumi: "<<phys.evt()<<" "<<phys.run()<<" "<<phys.lumi()<<endl;
       //if(phys.met_T1CHS_miniAOD_CORE_pt() >= 300){
       //  cout<<"Event: "<<phys.evt()<<" MET: "<<phys.met_T1CHS_miniAOD_CORE_pt()<<" njets: "<<phys.njets()<<" nbtags: "<<phys.nBJetMedium()<<" HT: "<<phys.ht()<<endl;
       //}
