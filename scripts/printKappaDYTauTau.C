7833ccc3264ba134a0038086b9dc43c6ec8e2f86
Bobak Hashemi <bth5032@gmail.com>
2017-01-01 16:38:49 -0800

    added little script to print dy -> tau tau numbers

--- scripts/printKappaDYTauTau.C
+++ scripts/printKappaDYTauTau.C
@@ -10,7 +10,7 @@ void printKappaDYTauTau(){
 
   fillFiles(on_files, above_files, below_files, "baseline_MET100_0B");
 
-  TH1D *onz_others, offz_others;
+  TH1D *onz_others, *offz_others;
   TH1D *onz_DY, *offz_DY;
 
   //Fill in DY histos with just DY data
